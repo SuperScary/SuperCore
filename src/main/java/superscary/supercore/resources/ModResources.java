80392850afd93970b47b986199d3db1a8d57c721
SuperScary (ERBF) <14338762+SuperScary@users.noreply.github.com>
2018-03-17 14:32:50 -0700

    Updated version

--- src/main/java/superscary/supercore/resources/ModResources.java
+++ src/main/java/superscary/supercore/resources/ModResources.java
@@ -20,7 +20,7 @@ public class ModResources
 
 	public static final String NAME = "SuperCore";
 	public static final String MODID = "supercore";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.3.1";
 	public static final String AUTHOR = "SuperScary (ERBF)";
 	public static final String DESCRIPTION = "Core mod for SuperScary's mods.";
 	public static final String CREDITS = "SuperScary (ERBF)";
